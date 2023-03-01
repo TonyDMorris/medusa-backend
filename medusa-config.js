98fed5e163800560f28df12daf3b49d508fcc57d
TonyDMorris <anthonymorris86@googlemail.com>
2023-03-01 21:49:17 +0000

    add conf

--- medusa-config.js
+++ medusa-config.js
@@ -61,10 +61,12 @@ module.exports = {
     // For more production-like environment install PostgresQL
     database_url: DATABASE_URL,
     database_type: "postgres",
+    
     // database_database: "./medusa-db.sql",
     // database_type: "sqlite",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
+    
   },
   plugins,
 };
